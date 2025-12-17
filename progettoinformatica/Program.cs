49246f66f0b48aecf6cae51a32d92093906b94a3
oscargagliardini <gagliardini.oscar@franchettisalviani.net>
2025-12-17 13:09:20 +0100

    Change break statement to return in Program.csffffff

--- progettoinformatica/Program.cs
+++ progettoinformatica/Program.cs
@@ -451,7 +451,7 @@ internal class Program
                     Console.WriteLine("   /                \\");
                     Console.WriteLine("  /                  \\");
                     Console.WriteLine(" /____________________\\");
-                    break;
+                    return;
                 }
 
 
